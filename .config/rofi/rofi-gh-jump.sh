2158bae60ae3550e4c7f2926ece25c2c68e808ee
Sergei Eremenko <finalitik@gmail.com>
2020-06-24 21:29:25 +0300

    rofi: rewrite rofi-gh-jump plugin to use offical gh command

--- .config/rofi/rofi-gh-jump.sh
+++ .config/rofi/rofi-gh-jump.sh
@@ -1,104 +1,31 @@
 #!/usr/bin/env bash
-# inspired by https://github.com/AlbertExtensions/Github-Jump
+# A Rofi plugin that allows a jump to your own, starred, and
+# subscribed GitHub repositories.
+# Inspired by https://github.com/AlbertExtensions/Github-Jump
+
+# Requires:
+#  - gh v0.10.1+ (https://github.com/cli/cli)
+#  - jq (https://github.com/stedolan/jq)
+
+# Copyright (c) 2020 Sergei Eremenko (https://github.com/SmartFinn)
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
+
 
 set -e
 
 : "${XDG_CACHE_HOME:=$HOME/.cache}"
 : "${UPDATE_CACHE:=0}"
 : "${GH_JUMP_CACHE="$XDG_CACHE_HOME/rofi-gh-jump.cache"}"
-
-_request() {
-	local path="${1:?Path is required.}"; shift 1
-	local api_url="https://api.github.com"
-
-	case "$path" in
-		http*) true ;;
-		/*) path="${api_url}${path}" ;;
-		*) path="${api_url}/${path}"
-	esac
-
-	curl -nsSig \
-		-H "Accept: application/vnd.github.v3+json" \
-		-H "Content-Type: application/json" \
-		"${path}"
-}
-
-_response() {
-	local -A headers=()
-	local http_version
-	local status_code
-	local status_text
-	local crlf
-
-	crlf=$(printf '\r\n')
-
-	read -r http_version status_code status_text
-
-	headers=(
-		[http_version]="${http_version#HTTP/}"
-		[status_code]="${status_code}"
-		[status_text]="${status_text%${crlf}}"
-	)
-
-	while IFS=": " read -r hdr val; do
-		# Headers stop at the first blank line.
-		[ "$hdr" = "$crlf" ] && break
-
-		val="${val%${crlf}}"
-
-		# Process each header
-		case "$hdr" in
-			Link)
-				# Split the URLs in the separate pseudo-headers
-				eval "$(printf '%s' "$val" | awk '
-					BEGIN { RS=", "; FS="; "; OFS="=" }
-					{
-						sub(/^rel="/, "", $2); sub(/"$/, "", $2)
-						sub(/^ *</, "", $1); sub(/>$/, "", $1)
-						print "headers[Link_" $2 "]", $1
-					}')"
-				continue
-				;;
-		esac
-
-		headers["$hdr"]="$val"
-
-	done
-
-	# Output requested headers in deterministic orde
-	for arg in "$@"; do
-		printf '%s\n' "${headers["$arg"]}"
-	done
-
-	cat
-}
-
-_get() {
-	local path="${1:?Path is required.}"; shift 1
-	local status_code
-	local status_text
-	local next_url
-
-	_request "$path" | _response status_code status_text Link_next | {
-		read -r status_code
-		read -r status_text
-		read -r next_url
-
-		case "$status_code" in
-			4*) printf 'Client Error: %s %s\n' \
-				"$status_code" "$status_text" 1>&2; exit 1 ;;
-			5*) printf 'Server Error: %s %s\n' \
-				"$status_code" "$status_text" 1>&2; exit 1 ;;
-		esac
-
-		# Output response body.
-		cat
-
-		if [ -n "$next_url" ]; then
-			_get "$next_url"
-		fi
-	}
-}
 
 open_in_browser() {
 	coproc xdg-open "https://github.com/$1" 2>&1
@@ -108,9 +35,9 @@ open_in_browser() {
 
 get_gh_repos() {
 	(
-		_get '/user/repos'
-		_get '/user/starred'
-		_get '/user/subscriptions'
+		command gh api --paginate user/repos &
+		command gh api --paginate user/starred &
+		command gh api --paginate user/subscriptions
 	) | jq -r '.[] | .full_name' | awk '!seen[$0]++'
 }
 
