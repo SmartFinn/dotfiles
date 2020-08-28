bc1ae8edf6e8dc086c8546eb6e5df2bf624e8b91
Sergei Eremenko <finalitik@gmail.com>
2020-08-28 21:12:38 +0300

    zsh: add zsh-hist and auto-hist-fix plugins
    Thanks to @marlonrichert for coolest zsh-hist plugin
    https://github.com/marlonrichert/zsh-hist

---
+++ .config/zsh/plugins/auto-hist-fix/auto-hist-fix.plugin.zsh
@@ -0,0 +1,26 @@
+# The plugin automatically run `hist f` if exit code of the last
+# command is not equal zero.
+#
+# requires https://github.com/marlonrichert/zsh-hist
+
+# exit immediately if hist function not exists
+typeset -f hist >/dev/null || return 0
+
+_auto_hist_fix() {
+	local last_exit_code="${(%)?}"
+
+	if [ "$last_exit_code" -ne 0 ]; then
+		# ignore SIGINT (Ctrl-C)
+		[ "$last_exit_code" -ne 130 ] || return 0
+
+		print -P "%F{blue}auto-hist-fix%f\n" \
+			"Press %F{green}Ctrl-Q%f to save the command in history" \
+			"without execution or %F{red}Ctrl-C%f to clear"
+		hist -s f -1
+	fi
+}
+
+bindkey '^[Q' push-line-or-edit
+
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd _auto_hist_fix
