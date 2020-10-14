363cb3565ad16b8fbbc9be9bf07a5fe5e5097753
Sergei Eremenko <finalitik@gmail.com>
2020-10-14 21:45:04 +0300

    zsh: improve zsh-abbr plugin

--- .config/zsh/plugins/zsh-abbrev/zsh-abbrev.plugin.zsh
+++ .config/zsh/plugins/zsh-abbrev/zsh-abbrev.plugin.zsh
@@ -24,9 +24,23 @@ no-magic-abbrev-expand() {
 	LBUFFER+=' '
 }
 
+magic-abbrev-expand-and-accept() {
+	local trailing_space
+	trailing_space=${LBUFFER##*[^[:IFSSPACE:]]}
+
+	if [[ -z $trailing_space ]]; then
+		LBUFFER=${LBUFFER%%(#m)[|:_a-zA-Z0-9]#}
+		LBUFFER+=${ZSH_ABBREVIATIONS[$MATCH]:-$MATCH}
+	fi
+
+	zle accept-line
+}
+
 zle -N magic-abbrev-expand
 zle -N no-magic-abbrev-expand
+zle -N magic-abbrev-expand-and-accept
 
 bindkey " " magic-abbrev-expand
 bindkey "^x " no-magic-abbrev-expand
+bindkey "^M" magic-abbrev-expand-and-accept
 bindkey -M isearch " " self-insert
