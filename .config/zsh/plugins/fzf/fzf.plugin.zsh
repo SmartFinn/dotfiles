6c7e6dd2eb06a6ea000ec91d258c42a8eb309830
Sergei Eremenko <finalitik@gmail.com>
2021-07-30 14:11:55 +0300

    zsh: enable completion trigger (**) in fzf plugin

--- .config/zsh/plugins/fzf/fzf.plugin.zsh
+++ .config/zsh/plugins/fzf/fzf.plugin.zsh
@@ -4,3 +4,6 @@ fi
 
 [ -f /usr/share/fzf/shell/key-bindings.zsh ] &&
 	source /usr/share/fzf/shell/key-bindings.zsh
+
+[ -f /usr/share/zsh/site-functions/fzf ] &&
+	source /usr/share/zsh/site-functions/fzf
