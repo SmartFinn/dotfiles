74741c84e92be70ed9fb321ff48df673f0181c1a
Sergei Eremenko <finalitik@gmail.com>
2022-09-18 12:49:44 +0300

    kitty: add sequence_keymap to list of key mapping. Add Kitten category

--- .config/kitty/keymap.py
+++ .config/kitty/keymap.py
@@ -7,14 +7,15 @@
 
 from kitty import fast_data_types
 from kitty.types import Shortcut
-import re
 from collections import OrderedDict
+import re
 
 # List of categories and regular expressions to match actions on
 categories = OrderedDict((
     ('Scrolling', r'(^scroll_|show_scrollback|show_last_command_output)'),
-    ('Tab Management', r'((^|_)tab(_|$)|next_layout)'),
+    ('Tab Management', r'((^|_)tab(_|$| )|next_layout)'),
     ('Window Management', r'(^|_)windows?(_|$)'),
+    ('Kitten', r'^kitten'),
     ('Other Shortcuts', r'.'),
 ))
 
@@ -23,13 +24,24 @@ def main(args):
     return ''
 
 
+def flatten_sequence_map(m):
+    ans = {}
+    for key_spec, rest_map in m.items():
+        for r, action in rest_map.items():
+            ans[Shortcut((key_spec,) + (r))] = action
+    return ans
+
+
 def handle_result(args, answer, target_window_id, boss):
     opts = fast_data_types.get_options()
     keymap = opts.keymap
 
     header = ["Kitty keyboard mappings", "=" * 23, ""]
     output_categorized = {}
+
     shortcuts = {Shortcut((k,)): v for k, v in keymap.items()}
+    shortcuts_seq = flatten_sequence_map(opts.sequence_map)
+    shortcuts.update(shortcuts_seq)
 
     for key, action in shortcuts.items():
         for category in categories:
