5501fc76458a448780c84d88a9f6eb9a29329072
Sergei Eremenko <finalitik@gmail.com>
2022-12-02 17:30:06 +0200

    Move comment to rigth context in dnf-check-update

--- .config/argos/dnf-check-update.10m.sh
+++ .config/argos/dnf-check-update.10m.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# --cacheonly option force to use system cache instead of creating a separate
-#             cache for each user under which it executes.
-#             Requires systemd unit dnf-makecache.timer enabled that also
-#             respects metered connection in Network Manager.
 
 mapfile -t -d $'\n' upgradable_packages < <(
 	# return app_name,version,repo
 	dnf --cacheonly check-update | awk -v OFS=',' 'NR>2 {print $1,$2,$3}'
+	# --cacheonly option force to use system cache instead of creating a
+	#             separate cache for each user under which it executes.
+	#             Requires systemd unit dnf-makecache.timer enabled that
+	#             also respects metered connection in Network Manager.
 )
 
 if [ "${#upgradable_packages[@]}" -gt 0 ]; then
