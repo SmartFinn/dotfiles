330a24013aa9ab5dc5bba19ac5ec4a3f1e8d5fcb
Sergei Eremenko <finalitik@gmail.com>
2023-02-04 21:55:42 +0200

    nvim: add nvim_lua to nvim-cmp sources

--- .config/nvim/lua/user/plugins/lsp/nvim-cmp.lua
+++ .config/nvim/lua/user/plugins/lsp/nvim-cmp.lua
@@ -90,6 +90,7 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'buffer', keyword_length = 3 },
     { name = 'luasnip', keyword_length = 2 },
+    { name = 'nvim_lua', keyword_length = 1 },
     { name = 'nvim_lsp', keyword_length = 3 },
     { name = 'path' },
   }),
