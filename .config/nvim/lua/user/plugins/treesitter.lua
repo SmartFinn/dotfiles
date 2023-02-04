547dafe5ed16dc20c6bad9cdbf0d097775d433a2
Sergei Eremenko <finalitik@gmail.com>
2023-02-04 21:54:17 +0200

    nvim: delegate smart rename feature to treesitter-refactor

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -135,6 +135,12 @@ require('nvim-treesitter.configs').setup({
   refactor = {
     highlight_definitions = { enable = true },
     -- highlight_current_scope = { enable = true },
+    smart_rename = {
+      enable = true,
+      keymaps = {
+        smart_rename = '<F2>',
+      },
+    },
   },
 })
 
