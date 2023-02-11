15955cb21361bde05deeb2063fd71b96884ab01e
Sergei Eremenko <finalitik@gmail.com>
2023-02-11 19:13:37 +0200

    devilspie2: improve placement of PiP windows

--- .config/devilspie2/on_window_focus.lua
+++ .config/devilspie2/on_window_focus.lua
@@ -1,11 +1,25 @@
-if (get_window_class() == "TelegramDesktop")
-or (get_window_class() == "Code")
-or (get_window_class() == "Meld")
-then
-	os.execute(string.format(
-		"wmctrl -r \"Picture in picture\" -e 0,%.0f,0,420,256", screen_x
-	));
-	os.execute(string.format(
-		"wmctrl -r \"Picture-in-picture\" -e 0,%.0f,0,420,256", screen_x
-	));
+local wm_class = get_window_class();
+local pip_windows = {"Picture in picture", "Picture-in-picture"};
+local wmctrl_mvargs = {
+	["Code"] = {screen_x, 180, 320, 180},
+	["Meld"] = {screen_x, screen_y - 210, 320, 180},
+	["TelegramDesktop"] = {screen_x, 0, 400, 226},
+	["Inkscape"] = {screen_x - 340, screen_y - 240, 280, 158},
+};
+
+if (get_window_type() == "WINDOW_TYPE_NORMAL") then
+	if (wmctrl_mvargs[wm_class] ~= nil) then
+		for _, name in ipairs(pip_windows) do
+			os.execute(string.format(
+				"wmctrl -r '%s' -e 0,%.0f,%.0f,%d,%d", name,
+					table.unpack(wmctrl_mvargs[wm_class])
+			));
+		end
+	else
+		for _, name in ipairs(pip_windows) do
+			os.execute(string.format(
+				"wmctrl -r '%s' -e 0,%.0f,%.0f,500,282", name, screen_x, screen_y
+			));
+		end
+	end
 end
