3fcaa7d7214fbcb3131b1c814f53cf2ac981339b
Sergei Eremenko <finalitik@gmail.com>
2023-02-12 14:06:05 +0200

    backintime: improve exclude_git_repos.sh to correct handle subdirs

--- .config/backintime/user-callback.d/exclude_git_repos.sh
+++ .config/backintime/user-callback.d/exclude_git_repos.sh
@@ -1,59 +1,78 @@
 #!/usr/bin/env bash
+# Generate list of local git repositories and uncommitted files
+# in rsync exclude file format
 
 set -eo pipefail
 
 reason="$3"
 
+case "$reason" in
+	1)  # Backup process begins
+		true
+		;;
+	*)  # Backup process ends
+		exit 0
+		;;
+esac
+
 IFS=$'\n\t'
 THIS_SCRIPT="${BASH_SOURCE[0]}"
-RSYNC_EXCLUDE_FILE="$HOME/.cache/backintime/git_repos.exclude"
+RSYNC_EXCLUDE_FILE="${XDG_CACHE_HOME:-$HOME/.cache}/backintime/git_repos.exclude"
 PROJECTS_DIRS=(
 	"$HOME"
-	# "$HOME/Git"
-	# "$HOME/Development"
+	"${XDG_CONFIG_HOME:-$HOME/.config}/"
+	"${XDG_DATA_HOME:-$HOME/.local}/share/"
 )
 
 mapfile -d $'\0' -t GIT_REPOS < <(
 	find "${PROJECTS_DIRS[@]}" -maxdepth 5 -type d -name '.git' -printf '%h\0'
 )
 
-staged_files() {
-	git -C "$1" ls-files 2>/dev/null \
-		| sed "s|^|$1/|" \
-		| sort
-}
-
-unstaged_files() {
-	git -C "$1" ls-files --exclude-standard --modified --others 2>/dev/null \
-		| sed "s|^|$1/|" \
-		| sort
-}
-
 generate_exclude_list_for_git_repo() {
 	local git_dir="$1"
+	local -a uncommitted_files
+	local -a path_components
+	local -i i j
 
 	# if has remotes
 	git -C "$git_dir" remote | grep -q '.' || return 0
-	# git -C "$git_dir" ls-remote -q &>/dev/null || return 0
 
-	mapfile -d $'\0' -t files < <(
+	mapfile -d $'\0' -t uncommitted_files < <(
 		git -C "$git_dir" ls-files -z --exclude-standard --others --modified 2>/dev/null
 	)
 
-	for file in "${files[@]}"; do
-		file_path="$git_dir/$file"
-		dir_path="${file_path%/*}"
+	# exclude whole git repo if array is empty
+	if [ "${#uncommitted_files[@]}" -eq 0 ]; then
+		printf -- '- %s/\n' "$git_dir"
+		return 0
+	fi
 
-		printf '1\t+ %s/\n' "${dir_path}"
-		printf '2\t+ %s\n' "${file_path}"
+	for uncommitted_file in "${uncommitted_files[@]}"; do
+		# Convert relative path to uncommitted file into bash array
+		IFS=/ read -ra path_components <<< "$uncommitted_file"
 
-		if [ "$dir_path" != "$git_dir" ]; then
-			printf '3\t- %s/*\n' "${file_path%/*}"
-		fi
-	done | sort -u | cut -f 2-
+		# for each path components except the last one
+		for ((i = 0; i < ${#path_components[@]} - 1; i++)) {
+			for ((j = 0; j <= i; j++)) {
+				subdir="${subdir:+$subdir/}${path_components[$j]}"
+			}
 
+			# cancel excluding subdirectory
+			printf -- '1\t+ %s/\n' "$git_dir/$subdir"
+
+			# exclude others files in the subdirectory
+			printf -- '2\t- %s/*\n' "$git_dir/$subdir"
+
+			unset subdir
+		}
+
+		# cancel excluding of uncommited files
+		printf -- '1\t+ %s\n' "$git_dir/$uncommitted_file"
+
+	done | sort -u | cut -f 2-  # remove duplicate lines
+
+	# exclude files in git work tree
 	printf -- '- %s/*\n' "$git_dir"
-	printf -- '- %s/\n'  "$git_dir"
 }
 
 generate_exclude_list() {
@@ -72,13 +91,6 @@ generate_exclude_list() {
 	done
 }
 
-case "$reason" in
-	1)  # Backup process begins
-		mkdir -p "${RSYNC_EXCLUDE_FILE%/*}"
-		rm -f "$RSYNC_EXCLUDE_FILE"
-		generate_exclude_list > "$RSYNC_EXCLUDE_FILE"
-		;;
-	2)  # Backup process ends
-		true
-		;;
-esac
+mkdir -p "${RSYNC_EXCLUDE_FILE%/*}"
+rm -f "$RSYNC_EXCLUDE_FILE"
+generate_exclude_list > "$RSYNC_EXCLUDE_FILE"
