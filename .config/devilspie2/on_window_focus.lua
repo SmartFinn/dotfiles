ecfee16ffcbd4584f6d5ba49d267531ebfc69dc3
Sergei Eremenko <finalitik@gmail.com>
2023-02-13 18:56:44 +0200

    devilspie2: improve management of PiP windows

--- .config/devilspie2/on_window_focus.lua
+++ .config/devilspie2/on_window_focus.lua
@@ -1,25 +1,30 @@
 local wm_class = get_window_class();
 local pip_windows = {"Picture in picture", "Picture-in-picture"};
+local pip_preset = "_window";
 local wmctrl_mvargs = {
+	["_window"] = {screen_x, screen_y, 500, 282},
+	["_maximized"] = {screen_x, screen_y, 320, 180},
 	["Code"] = {screen_x, 180, 320, 180},
 	["Meld"] = {screen_x, screen_y - 210, 320, 180},
 	["TelegramDesktop"] = {screen_x, 0, 400, 226},
 	["Inkscape"] = {screen_x - 340, screen_y - 240, 280, 158},
+	["firefox"] = {screen_x, screen_y, 320, 180},
+	["org-openstreetmap-josm-gui-MainApplication"] = {
+		screen_x - 520, screen_y, 320, 180
+	},
 };
 
 if (get_window_type() == "WINDOW_TYPE_NORMAL") then
 	if (wmctrl_mvargs[wm_class] ~= nil) then
-		for _, name in ipairs(pip_windows) do
-			os.execute(string.format(
-				"wmctrl -r '%s' -e 0,%.0f,%.0f,%d,%d", name,
-					table.unpack(wmctrl_mvargs[wm_class])
-			));
-		end
-	else
-		for _, name in ipairs(pip_windows) do
-			os.execute(string.format(
-				"wmctrl -r '%s' -e 0,%.0f,%.0f,500,282", name, screen_x, screen_y
-			));
-		end
+		pip_preset = wm_class;
+	elseif (get_window_is_maximized()) then
+		pip_preset = "_maximized";
+	end
+
+	for _, name in ipairs(pip_windows) do
+		os.execute(string.format(
+			"wmctrl -r '%s' -e 0,%.0f,%.0f,%d,%d", name,
+				table.unpack(wmctrl_mvargs[pip_preset])
+		));
 	end
 end
