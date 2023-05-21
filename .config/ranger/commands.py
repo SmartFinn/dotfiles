c6f7f3846a99cd7b92d5c67ae638eba9562debc9
Sergei Eremenko <finalitik@gmail.com>
2023-05-21 09:15:03 +0300

    ranger: do not use wl-copy in X11 session

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -248,3 +248,73 @@ class empty_trash(Command):
     def _question_callback(self, answer):
         if answer == 'y' or answer == 'Y':
             self.fm.run(['gio', 'trash', '--empty'])
+
+
+class yank(Command):
+    """:yank [name|dir|path|name_without_extension]
+
+    Copies the file's name (default), directory or path into both the primary X
+    selection and the clipboard.
+    """
+
+    modes = {
+        '': 'basename',
+        'name_without_extension': 'basename_without_extension',
+        'name': 'basename',
+        'dir': 'dirname',
+        'path': 'path',
+    }
+
+    def execute(self):
+        import subprocess
+
+        def clipboards():
+            from ranger.ext.get_executables import get_executables
+            clipboard_managers = {
+                'xclip': [
+                    ['xclip'],
+                    ['xclip', '-selection', 'clipboard'],
+                ],
+                'xsel': [
+                    ['xsel'],
+                    ['xsel', '-b'],
+                ],
+                'wl-copy': [
+                    ['wl-copy'],
+                ],
+                'pbcopy': [
+                    ['pbcopy'],
+                ],
+            }
+            _is_wayland_display=os.environ.get('WAYLAND_DISPLAY', None)
+            if _is_wayland_display:
+                return clipboard_managers['wl-copy']
+            ordered_managers = ['pbcopy', 'xclip', 'xsel']
+            executables = get_executables()
+            for manager in ordered_managers:
+                if manager in executables:
+                    return clipboard_managers[manager]
+            return []
+
+        clipboard_commands = clipboards()
+
+        mode = self.modes[self.arg(1)]
+        selection = self.get_selection_attr(mode)
+
+        new_clipboard_contents = "\n".join(selection)
+        for command in clipboard_commands:
+            with subprocess.Popen(
+                command, universal_newlines=True, stdin=subprocess.PIPE
+            ) as process:
+                process.communicate(input=new_clipboard_contents)
+
+    def get_selection_attr(self, attr):
+        return [getattr(item, attr) for item in
+                self.fm.thistab.get_selection()]
+
+    def tab(self, tabnum):
+        return (
+            self.start(1) + mode for mode
+            in sorted(self.modes.keys())
+            if mode
+        )
