5c1b58e802b5f866688f53b266b6fa5b9997b042
Sergei Eremenko <finalitik@gmail.com>
2023-08-15 15:15:09 +0300

    nvim: add separate ftplugin for checkhealth, and symlink for :InspectTree

--- .config/nvim/after/ftplugin/checkhealth.lua
+++ .config/nvim/after/ftplugin/checkhealth.lua
@@ -1,1 +1,27 @@
-help.lua
+-- help specific settings.
+-- Options
+vim.opt_local.tabstop = 8
+vim.opt_local.shiftwidth = 8
+vim.opt_local.shiftround = false
+vim.opt_local.expandtab = false
+vim.opt_local.foldmethod = "indent"
+vim.opt_local.wrap = true
+vim.opt_local.cursorcolumn = false
+vim.opt_local.number = false
+vim.opt_local.foldenable = false
+vim.opt_local.relativenumber = false
+vim.opt_local.colorcolumn = "0"
+vim.opt_local.textwidth = 0
+
+-- Compiler
+
+-- Syntax
+
+-- Macros
+
+-- Mapping
+vim.keymap.set('n', 'q', '<CMD>bdelete<CR>', { buffer = true })
+vim.keymap.set('n', '<CR>', '<C-]>', { buffer = true })
+vim.keymap.set('n', '<BS>', '<C-T>', { buffer = true })
+
+-- Plugins
