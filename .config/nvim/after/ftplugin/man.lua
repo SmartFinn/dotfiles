1e77a7df38a87ea24d52a9cc00d0758b1834a022
Sergei Eremenko <finalitik@gmail.com>
2023-08-21 12:08:36 +0300

    nvim: add separate ftplugin for man pages

--- .config/nvim/after/ftplugin/man.lua
+++ .config/nvim/after/ftplugin/man.lua
@@ -1,1 +1,25 @@
-help.lua
+-- help specific settings.
+-- Options
+vim.opt_local.tabstop = 8
+vim.opt_local.shiftwidth = 8
+vim.opt_local.shiftround = false
+vim.opt_local.expandtab = false
+vim.opt_local.foldmethod = "indent"
+vim.opt_local.wrap = false
+vim.opt_local.cursorcolumn = false
+vim.opt_local.number = false
+vim.opt_local.foldenable = false
+vim.opt_local.relativenumber = false
+vim.opt_local.colorcolumn = "0"
+vim.opt_local.textwidth = 0
+
+-- Compiler
+
+-- Macros
+
+-- Mapping
+vim.keymap.set('n', 'q', '<CMD>bdelete<CR>', { buffer = true, silent = true })
+vim.keymap.set('n', '<CR>', '<C-]>', { buffer = true, silent = true })
+vim.keymap.set('n', '<BS>', '<C-T>', { buffer = true, silent = true })
+
+-- Plugins
