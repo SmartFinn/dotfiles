5eee1c014f71fec5c201f9ade4ad8df0f4425de4
Serhii Yeremenko <finalitik@gmail.com>
2024-01-10 08:55:12 +0200

    nvim: replace vim alias with standalone wrapper

--- .config/shell.d/15_nvim.sh
+++
@@ -1,9 +0,0 @@
-command -v nvim >/dev/null || return 0
-
-if ! command -v vim >/dev/null; then
-	alias vim="NVIM_APPNAME=vim nvim"
-fi
-
-if [ -d "${XDG_CONFIG_HOME:-$HOME/.config}/astronvim" ]; then
-	alias astronvim="NVIM_APPNAME=astronvim nvim"
-fi
