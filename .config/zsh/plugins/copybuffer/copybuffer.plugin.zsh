bbb17f02a35ade9e89a4b6422ed86b27f197a882
Serhii Yeremenko <finalitik@gmail.com>
2024-05-29 21:40:49 +0300

    zsh: rename clip-tools plugin to copybuffer

--- .config/zsh/plugins/copybuffer/copybuffer.plugin.zsh
+++ .config/zsh/plugins/copybuffer/copybuffer.plugin.zsh
@@ -1,10 +1,10 @@
 # Copy current command to clipboard (Ctrl+X c)
 # https://www.reddit.com/r/commandline/comments/4fjpb0/question_how_to_copy_the_command_to_clipboard/
 
-buffer_to_clipboard() {
+copybuffer() {
 	printf '%s' "$BUFFER" | clipcopy
 	zle -M "📋Copied the buffer to clipboard."
 }
 
-zle -N buffer_to_clipboard
-bindkey '^Xc' buffer_to_clipboard
+zle -N copybuffer
+bindkey '^Xc' copybuffer
