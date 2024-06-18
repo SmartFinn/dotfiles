08603dd5053f2d786cd931ce3983925e81403944
Serhii Yeremenko <finalitik@gmail.com>
2024-06-18 20:15:34 +0300

    kitty: fix keymap.py plugin for kitty v0.32.0+

--- .config/kitty/plugins/keymap.py
+++ .config/kitty/plugins/keymap.py
@@ -5,8 +5,10 @@
 # Place this in ~/.config/kitty/keymap.py then
 # Add "map kitty_mod+/ kitten keymap.py" to your kitty.conf
 
+from kittens.tui.handler import result_handler
 from kitty import fast_data_types
-from kitty.types import Shortcut
+from kitty.types import Shortcut, mod_to_names
+from kitty.boss import Boss
 from collections import OrderedDict
 import re
 
@@ -16,32 +18,31 @@ categories = OrderedDict((
     ('Tab Management', r'((^|_)tab(_|$| )|next_layout)'),
     ('Window Management', r'(^|_)windows?(_|$)'),
     ('Kitten', r'^kitten'),
+    ('Launch', r'^launch'),
     ('Other Shortcuts', r'.'),
 ))
 
 
-def main(args):
-    return ''
+def main(args: list[str]) -> None:
+    pass
 
 
-def flatten_sequence_map(m):
-    ans = {}
-    for key_spec, rest_map in m.items():
-        for r, action in rest_map.items():
-            ans[Shortcut((key_spec,) + (r))] = action
+def flatten_sequence_map(keymap: dict) -> dict:
+    ans: dict[Shortcut, str] = {}
+    for key_spec, defns in keymap.items():
+        for d in defns:
+            ans[Shortcut(d.full_key_sequence_to_trigger)] = d.human_repr()
     return ans
 
 
-def handle_result(args, answer, target_window_id, boss):
+@result_handler(no_ui=True)
+def handle_result(args: list[str], answer: str, target_window_id: int, boss:
+                  Boss):
     opts = fast_data_types.get_options()
-    keymap = opts.keymap
+    keymap = opts.keyboard_modes[''].keymap
 
-    header = ["Kitty keyboard mappings", "=" * 23, ""]
-    output_categorized = {}
-
-    shortcuts = {Shortcut((k,)): v for k, v in keymap.items()}
-    shortcuts_seq = flatten_sequence_map(opts.sequence_map)
-    shortcuts.update(shortcuts_seq)
+    output_categorized: dict = {}
+    shortcuts: dict = flatten_sequence_map(keymap)
 
     for key, action in shortcuts.items():
         for category in categories:
@@ -49,14 +50,21 @@ def handle_result(args, answer, target_window_id, boss):
                 break
 
         output_categorized.setdefault(category, []).append(
-            f'{key.human_repr(opts.kitty_mod)} → {action}')
+            f'{key.human_repr(opts.kitty_mod):<20} → {action}')
 
-    output = header.copy()
+    output: list[str] = [
+        "Kitty keyboard mappings",
+        "=======================",
+        "",
+        "My kitty_mod is {}.".format("+".join(mod_to_names(opts.kitty_mod))),
+        "",
+    ]
     for category in categories:
-        if category in output_categorized:
-            output.extend([category, "=" * len(category), ""])
-            output.extend(output_categorized[category])
-            output.append("")
+        if category not in output_categorized:
+            continue
+        output.extend([category, "=" * len(category), ""])
+        output.extend(output_categorized[category])
+        output.append("")
 
     boss.display_scrollback(boss.active_window, "\n".join(output[:-1]),
                             title="Kitty keyboard mappings",
