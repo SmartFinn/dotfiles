89d51e71603e1ea1c63ece1576b8fee6adc72a8c
Serhii Yeremenko <finalitik@gmail.com>
2024-12-03 20:22:15 +0200

    devilspie2: disable PiP manipulation when the second screen attached

--- .config/devilspie2/on_window_focus.lua
+++ .config/devilspie2/on_window_focus.lua
@@ -1,4 +1,6 @@
 table.unpack = table.unpack or unpack -- 5.1 compatibility
+local screen_x, screen_y = get_screen_geometry();
+local aspect_ratio = screen_x / screen_y;
 local wm_class = get_window_class();
 local wm_role = get_window_role();
 local pip_windows = {"Picture in picture", "Picture-in-picture"};
@@ -23,15 +25,18 @@ if (get_window_type() == "WINDOW_TYPE_NORMAL") then
 	if (wmctrl_mvargs[wm_class] ~= nil) then
 		pip_preset = wm_class;
 	elseif (wmctrl_mvargs[wm_role] ~= nil) then
-	  pip_preset = wm_role;
+		pip_preset = wm_role;
 	elseif (get_window_is_maximized()) then
 		pip_preset = "_maximized";
 	end
 
-	for _, name in ipairs(pip_windows) do
-		os.execute(string.format(
-			"wmctrl -r '%s' -e 0,%.0f,%.0f,%d,%d", name,
-				table.unpack(wmctrl_mvargs[pip_preset])
-		));
+	if (aspect_ratio > 1.0 and aspect_ratio < 2.0) then
+		-- Disable if ultrawide, ultrahigh, or the second screen is attached
+		for _, name in ipairs(pip_windows) do
+			os.execute(string.format(
+				"wmctrl -r '%s' -e 0,%.0f,%.0f,%d,%d", name,
+					table.unpack(wmctrl_mvargs[pip_preset])
+			));
+		end
 	end
 end
--- .config/devilspie2/on_window_open.lua
+++ .config/devilspie2/on_window_open.lua
@@ -4,7 +4,9 @@ local wm_role = get_window_role();
 local wm_class = get_window_class();
 local inst_name = get_class_instance_name() or "";
 local screen_x, screen_y = get_screen_geometry();
+local aspect_ratio = screen_x / screen_y;
 
+debug_print("Screen geomentry: " .. screen_x .. ", " .. screen_y)
 debug_print("Application name: " .. app_name);
 debug_print("Window class: "     .. wm_class);
 debug_print("Instance name: "    .. inst_name);
@@ -48,7 +50,11 @@ if (get_window_type() == "WINDOW_TYPE_NORMAL") then
 end
 
 if (app_name == 'Picture in picture' or wm_role == 'PictureInPicture') then
-	set_window_geometry(screen_x, screen_y, screen_x / 2.72, screen_y / 2.72);
+	if (aspect_ratio > 1.0 and aspect_ratio < 2.0) then
+		set_window_geometry(screen_x, screen_y, screen_x / 2.72, screen_y / 2.72);
+	else
+		set_window_geometry(screen_x, screen_y, 480, 320);
+	end
 	stick_window();
 	-- set_skip_tasklist(true);
 	set_skip_pager(true);
