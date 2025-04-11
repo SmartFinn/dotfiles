897fb8a0dbb8b4bfc0c13be89de65f27aa8c16ea
Serhii Yeremenko <finalitik@gmail.com>
2025-04-11 20:05:35 +0300

    nvim: refactoring onedark plugin setup

--- .config/nvim/lua/plugins/onedark.lua
+++ .config/nvim/lua/plugins/onedark.lua
@@ -5,22 +5,19 @@ return {
   'navarasu/onedark.nvim',
   lazy = false,
   priority = 1000,
-  config = function ()
-    local onedark = require("onedark")
-
-    onedark.setup({
-      style = 'darker',
+  opts = {
+    style = 'darker',
+    transparent = false,
+    lualine = {
       transparent = false,
-      lualine = {
-        transparent = false,
-      },
-      highlights = {
-        ['LineNr'] = {fg = '$bg3'},
-        ['NormalFloat'] = {fg = '$fg', bg = '$bg0'},
-        ['FloatBorder'] = {fg = '$grey', bg = '$bg0'},
-      },
-    })
-
-    onedark.load()
-  end,
+    },
+    highlights = {
+      ['LineNr'] = {fg = '$bg3'},
+      ['NormalFloat'] = {fg = '$fg', bg = '$bg0'},
+      ['FloatBorder'] = {fg = '$grey', bg = '$bg0'},
+    },
+  },
+  init = function()
+    require('onedark').load()
+  end
 }
