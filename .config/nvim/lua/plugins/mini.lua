fea456001f98928f6ffa583953afe1fd522a4e3c
Serhii Yeremenko <finalitik@gmail.com>
2025-04-14 08:28:38 +0300

    nvim: add mini.ai plugin

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,12 @@
+-- Mini.nvim library
+-- https://github.com/echasnovski/mini.nvim
+
+return {
+  -- https://github.com/echasnovski/mini.ai
+  {
+    'echasnovski/mini.ai',
+    event = 'VeryLazy',
+    version = '*',
+    opts={}
+  },
+}
