dfd704e4c1fb12846d5f58fefa5022bbfadf7c82
Serhii Yeremenko <finalitik@gmail.com>
2025-05-18 22:26:56 +0300

    shell.d: improve GPG_TTY handling

--- .config/shell.d/15_gpg.sh
+++ .config/shell.d/15_gpg.sh
@@ -1,4 +1,11 @@
 command -v gpg >/dev/null || return 0
 
-GPG_TTY="$(tty)"
+# Set the GPG_TTY to be the same as the TTY, either via the env var
+# or via the tty command.
+if [ -n "$TTY" ]; then
+	GPG_TTY=$(tty)
+else
+	GPG_TTY="$TTY"
+fi
+
 export GPG_TTY
