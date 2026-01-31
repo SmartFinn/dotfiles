10ffcb94c66768ed939c97703a6fa2ccd2ebeba4
Serhii Yeremenko <finalitik@gmail.com>
2026-01-31 20:30:51 +0200

    profile.d: move profile.d/* files to shell.d/*

--- .config/shell.d/init.sh
+++ .config/shell.d/init.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+path_missing() {
+	# usage: path_missing "$PATH" "/usr/local/bin"
+	case ":$1:" in
+		*:"${2%/}":*)
+			false
+			;;
+		*)
+			true
+	esac
+}
+
 test_glob() {
 	# usage: test_glob ":$PATH:" "*:/usr/bin:*"
 	case "$1" in
@@ -15,4 +26,5 @@ for i in "${XDG_CONFIG_HOME:-$HOME/.config}"/shell.d/??_*.sh; do
 done
 
 unset i
+unset -f path_missing
 unset -f test_glob
